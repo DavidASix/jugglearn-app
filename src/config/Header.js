e7043656126b93c3ca7784165f00dcc4ccef1db0
davidasix <davidandersonsix@gmail.com>
2022-08-10 21:50:55 -0400

    Prettier formatted config details

--- src/config/Header.js
+++ src/config/Header.js
@@ -5,10 +5,10 @@ import {
   Text,
   Image,
   TouchableOpacity,
-  Animated
+  Animated,
 } from 'react-native';
 //import MCIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import * as actions from '../actions';
 
 import c from '../assets/constants';
@@ -22,30 +22,39 @@ const Header = () => {
   const iconSize = 90;
   return (
     <View style={styles.headerContainer}>
-
       <View style={styles.angledBg} />
 
       <View style={styles.headerContentRow}>
-        <View style={{ position: 'relative', height: '100%', width: iconSize  }}>
+        <View style={{position: 'relative', height: '100%', width: iconSize}}>
           <Image
             source={require('../assets/icons/icon.png')}
             defaultSource={require('../assets/icons/icon.png')}
-            style={{ height: iconSize, width: iconSize, position: 'absolute' }}
+            style={{height: iconSize, width: iconSize, position: 'absolute'}}
             resizeMethod="scale"
-            resizeMode="cover" />
+            resizeMode="cover"
+          />
         </View>
-        <Text style={{ fontSize: 38, alignSelf: 'center', marginTop: 20, fontFamily: "Boiling", color: c.colors.text.dark }}>
+        <Text
+          style={{
+            fontSize: 38,
+            alignSelf: 'center',
+            marginTop: 20,
+            fontFamily: 'Boiling',
+            color: c.colors.text.dark,
+          }}
+        >
           Jugglearn
         </Text>
         <SVGIcon
-          name='gear'
+          name="gear"
           size={35}
           color={c.colors.text.dark}
-          style={{ margin: 20 }} />
+          style={{margin: 20}}
+        />
       </View>
     </View>
   );
-}
+};
 
 const styles = {
   headerContainer: {
@@ -54,7 +63,7 @@ const styles = {
     zIndex: 99,
     padding: 5,
     elevation: 10,
-    backgroundColor: 'red'
+    //backgroundColor: 'red',
   },
   headerContentRow: {
     elevation: 10,
@@ -67,7 +76,7 @@ const styles = {
   },
   angledBg: {
     position: 'absolute',
-    shadowColor: "#000",
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
       height: 1,
@@ -76,11 +85,12 @@ const styles = {
     shadowRadius: 2.22,
     bottom: -16,
     left: -20,
-    height: '200%', width: '120%',
+    height: '200%',
+    width: '120%',
     backgroundColor: '#F3f3f3',
     opacity: 1,
     elevation: 10,
-    transform: [{ rotate: '-5deg' }]
+    transform: [{rotate: '-5deg'}],
   },
 };
 
--- src/config/Router.js
+++ src/config/Router.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import {
-  BackHandler,
-} from 'react-native';
+import {BackHandler} from 'react-native';
 
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import * as actions from '../actions';
 
-import { NavigationContainer, useRoute } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator, TransitionPresets  } from '@react-navigation/stack';
+import {NavigationContainer, useRoute} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
 
 import c from '../assets/constants';
 
@@ -27,14 +25,19 @@ import Header from './Header';
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
-function mapStateToProps ({ settings }) {
-  return { theme: settings.theme };
-};
+function mapStateToProps({settings}) {
+  return {theme: settings.theme};
+}
 /** TAB NAVIGATOR **/
 
 const Tabs = () => {
   React.useEffect(() => {
-    const backHandler = BackHandler.addEventListener("hardwareBackPress", () => { return true; });
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      () => {
+        return true;
+      },
+    );
     return () => backHandler.remove();
   }, []);
   // headerStyle for Header Heigh
@@ -42,60 +45,87 @@ const Tabs = () => {
     <Tab.Navigator
       initialRouteName="courses"
       tabBar={props => <TabBar {...props} />}
-      screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="courses" component={Courses} options={{ title: 'Courses' }} />
-      <Tab.Screen name="exercises" component={Exercises} options={{ title: 'Exercises' }} />
-      <Tab.Screen name="profile" component={Profile} options={{ title: 'My Profile' }} />
-      <Tab.Screen name="purchases" component={Purchases} options={{ title: 'Purchases' }} />
+      screenOptions={{headerShown: false}}
+    >
+      <Tab.Screen
+        name="courses"
+        component={Courses}
+        options={{title: 'Courses'}}
+      />
+      <Tab.Screen
+        name="exercises"
+        component={Exercises}
+        options={{title: 'Exercises'}}
+      />
+      <Tab.Screen
+        name="profile"
+        component={Profile}
+        options={{title: 'My Profile'}}
+      />
+      <Tab.Screen
+        name="purchases"
+        component={Purchases}
+        options={{title: 'Purchases'}}
+      />
     </Tab.Navigator>
   );
-}
+};
 
 /** MODAL STACK NAVIGATOR **/
 const ModalStack = () => {
   return (
     <Stack.Navigator
-      screenOptions={({ route, navigation }) => ({
+      screenOptions={({route, navigation}) => ({
         headerShown: true,
         gestureEnabled: true,
         presentation: 'transparentModal',
         ...TransitionPresets.ModalSlideFromBottomIOS,
-        header: Header
+        header: Header,
       })}
-      initialRouteName="courses" >
+      initialRouteName="courses"
+    >
       <Stack.Screen
         name="tabs"
         component={Tabs}
-        options={({ navigation, route }) => ({ title: 'Courses' })} />
+        options={({navigation, route}) => ({title: 'Courses'})}
+      />
       <Stack.Screen
         name="exerciseVideo"
         component={ExerciseVideo}
-        options={({ navigation, route }) => ({
+        options={({navigation, route}) => ({
           title: 'Video',
-          headerShown: false
-        })} />
+          headerShown: false,
+        })}
+      />
     </Stack.Navigator>
   );
-}
+};
 
 /** AUTH NAVIGATOR **/
 
 const AuthStack = () => {
   return (
     <Stack.Navigator
-      screenOptions={{ headerShown: false, gestureEnabled: false, ...TransitionPresets.ModalSlideFromBottomIOS  }}
-      initialRouteName="modalStack" >
+      screenOptions={{
+        headerShown: false,
+        gestureEnabled: false,
+        ...TransitionPresets.ModalSlideFromBottomIOS,
+      }}
+      initialRouteName="modalStack"
+    >
       <Stack.Screen
         name="authentication"
         component={Authentication}
-        options={({ navigation, route }) => ({ title: 'Authentication' })} />
+        options={({navigation, route}) => ({title: 'Authentication'})}
+      />
       <Stack.Screen
         name="modalStack"
         component={ModalStack}
-        options={({ navigation, route }) => ({ title: 'modalStack' })} />
+        options={({navigation, route}) => ({title: 'modalStack'})}
+      />
     </Stack.Navigator>
   );
-}
+};
 
 const Router = () => {
   return (
@@ -103,6 +133,6 @@ const Router = () => {
       <AuthStack />
     </NavigationContainer>
   );
-}
+};
 
 export default Router;
